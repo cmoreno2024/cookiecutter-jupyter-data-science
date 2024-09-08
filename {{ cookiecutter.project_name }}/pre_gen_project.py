fecee8fdeb782da38613c8aaefee76020163213e
Diego Lerma <dieglt_98@hotmail.com>
2024-09-08 15:38:20 -0600

    Automatizo creacion de entornos virtuales

--- {{ cookiecutter.project_name }}/pre_gen_project.py
+++ {{ cookiecutter.project_name }}/pre_gen_project.py
@@ -1,6 +1,15 @@
-# hooks/pre_gen_project.py
 from datetime import datetime
 import os
+import subprocess
+import sys
+import re
+
+
+# Función para validar el nombre del proyecto
+def validar_nombre_proyecto(name):
+    if not re.match(r'^[a-zA-Z0-9_-]+$', name):
+        raise ValueError(f"Nombre del proyecto no válido: {name}")
+    return name
 
 
 # Función principal del pre-hook
@@ -24,3 +33,37 @@ def pre_gen_project():
         f.seek(0)
         f.write(content)
         f.truncate()
+
+    # Crear el entorno virtual con Mamba
+    project_name = validar_nombre_proyecto('{{ cookiecutter.project_slug }}')
+    python_version = '{{ cookiecutter.python_version }}'
+
+    # Validar versión de Python (opcional, solo números y puntos)
+    if not re.match(r'^\d+(\.\d+){0,2}$', python_version):
+        raise ValueError(f"Versión de Python no válida: {python_version}")
+
+    print(f"Creando el entorno virtual '{project_name}' con Mamba...")
+    create_env_cmd = ['mamba', 'create', '-n',
+                      project_name, f'python={python_version}', '-y']
+
+    try:
+        # Ejecutar el comando para crear el entorno virtual sin shell=True
+        subprocess.run(create_env_cmd, check=True)
+        print(f"Entorno virtual '{project_name}' creado exitosamente.")
+
+        # Instalar dependencias desde environment.yml si el archivo existe
+        env_file = 'environment.yml'
+        if os.path.exists(env_file):
+            print(f"Instalando dependencias desde {env_file}...")
+            install_deps_cmd = ['mamba', 'env',
+                                'update', '-n', project_name, '-f', env_file]
+            subprocess.run(install_deps_cmd, check=True)
+            print("Dependencias instaladas correctamente.")
+
+    except subprocess.CalledProcessError as error:
+        print(f"Error al crear el entorno o instalar dependencias: {error}")
+        sys.exit(1)
+
+    # Mostrar instrucciones para activar el entorno
+    print("Para activar el entorno, usa:")
+    print(f"    conda activate {project_name}")
